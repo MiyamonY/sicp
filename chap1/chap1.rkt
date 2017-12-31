33d8f8ea6d9431aa8c5718ea6f0e4591553e3d6f
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2018-01-01 03:24:40 +0900

    [add] 1.1.6 条件式と述語 まで読んだ

--- chap1/chap1.rkt
+++ chap1/chap1.rkt
@@ -27,3 +27,40 @@
 
 (module+ test
   (check-eq? (f 5) 136))
+
+;; 1.1.6 条件式と述語
+
+(define (abs x)
+  (cond ((> x 0) x)
+        ((= x 0) 0)
+        ((< x 0) (- x))))
+
+(module+ test
+  (check-eq? (abs 3) 3)
+  (check-eq? (abs 0) 0)
+  (check-eq? (abs -5) 5))
+
+(define (abs2 x)
+  (cond ((< x 0) (- x))
+        (else  x)))
+
+(module+ test
+  (check-eq? (abs2 2) 2)
+  (check-eq? (abs2 -3) 3))
+
+(define (abs3 x)
+  (if (< x 0)
+      (- x)
+      x))
+
+(module+ test
+  (check-eq? (abs3 1) 1)
+  (check-eq? (abs3 -6) 6))
+
+(define (>= x y)
+  (not (< x y)))
+
+(module+ test
+  (check-true (>= 3 3))
+  (check-true (>= 5 2))
+  (check-false (>= 2 4)))
