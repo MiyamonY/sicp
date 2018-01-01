8d97a752f4fc449a3be1be56c225be1a30b132f8
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2018-01-01 16:48:41 +0900

    [add] 1.1.7 例:Newton法による平方根 まで読んだ

--- chap1/chap1.rkt
+++ chap1/chap1.rkt
@@ -64,3 +64,26 @@
   (check-true (>= 3 3))
   (check-true (>= 5 2))
   (check-false (>= 2 4)))
+
+;; 1.1.7 例.Newton法による平方根
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x)
+                 x)))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+(module+ test
+  (check-= (sqrt 9) 3.0 0.0001)
+  (check-= (sqrt (+ 100 37)) 11.704699 0.001))
