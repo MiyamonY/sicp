7ef803ac8d425fc6bcf6356a0ff005c50b17e22a
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2018-01-04 00:40:01 +0900

    [add] 1.2.2 木構造再帰 まで読んだ

--- chap1/chap1.rkt
+++ chap1/chap1.rkt
@@ -123,3 +123,44 @@
 
 (module+ test
   (check-eq? (ifactorial 4) 24))
+
+;; 1.2.2 木構造再帰
+(define (fib n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fib (- n 1))
+                 (fib (- n 2))))))
+
+(define (fib2 n)
+  (define (fib-iter a b count)
+    (if (= count 0)
+        b
+        (fib-iter (+ a b) a (- count 1))))
+  (fib-iter 1 0 n))
+
+(module+ test
+  (check-eq? (fib 3) (fib2 3))
+  (check-eq? (fib 7) (fib2 7)))
+
+(define (count-change amount)
+  (cc amount 5))
+
+(define (cc amount kinds-of-coins)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (= kinds-of-coins 0)) 0)
+        (else (+ (cc amount
+                     (- kinds-of-coins 1))
+                 (cc (- amount (first-denomination kinds-of-coins))
+                     kinds-of-coins)))))
+
+(define (first-denomination kinds-of-coins)
+  (cond ((= kinds-of-coins 1) 1)
+        ((= kinds-of-coins 2) 5)
+        ((= kinds-of-coins 3) 10)
+        ((= kinds-of-coins 4) 25)
+        ((= kinds-of-coins 5) 50)))
+
+(module+ test
+  (check-eq? (count-change 0) 1)
+  (check-eq? (count-change 3) 1)
+  (check-eq? (count-change 15) 6))
