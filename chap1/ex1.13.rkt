ce2e626148f6f3bfaf163310f404ec0186714190
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2018-01-04 14:44:35 +0900

    [add] 問題1.13 を追加

---
+++ chap1/ex1.13.rkt
@@ -0,0 +1,14 @@
+;;;
+;; File:  ex1.13.rkt
+;; Author: ymiyamoto
+;;
+;; Created on Thu Jan  4 14:28:13 2018
+;;
+#lang racket
+
+;; 帰納法と φ**2 = φ + 1の関係を使えば良い。
+;; 任意の n について ψ ** n / sqrt(5) < 0.5より、示せる。
+
+(module+ test
+  (require rackunit)
+  )
