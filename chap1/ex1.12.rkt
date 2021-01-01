fce3646042d582a079df065a3681c8f8534d967d
MIYAMOTO Yohei <miyamoto@heartorg.co.jp>
2021-01-01 23:12:31 +0900

    [Add] ex1.12

--- chap1/ex1.12.rkt
+++ chap1/ex1.12.rkt
@@ -1,22 +1,22 @@
 #lang racket
 
-(define (pow e n)
-  (if (= n 0)
-      1
-      (* e (pow e (- n 1)))))
-
-(define (phi n)
-  (pow (/ (+ 1 (sqrt 5)) 2) n))
-
-(define (psi n)
-  (pow (/ (- 1 (sqrt 5)) 2) n))
-
-(define (fib n)
-  (round (/ (phi n) (sqrt 5))))
+(define (pascal n k)
+  (cond ((<  n k) 0)
+        ((= n 1) 1)
+        ((or (= k 1) (= n k)) 1)
+        (else
+         (+ (pascal (- n 1) (- k 1))
+            (pascal (- n 1) k)))))
 
 (module+ test
   (require rackunit)
 
-  ;; phi^2 = (3+sqrt 5)/ 2 より Fib(n) = phi^n - psi^n / sqrt 5が言える．
-  ;; abs (psi^n) < 1より，題意が示せた
-  (check-true (< (abs (psi 1)) 1.0)))
+  (check-equal? (pascal 1 1) 1)
+  (check-equal? (pascal 2 1) 1)
+  (check-equal? (pascal 2 2) 1)
+  (check-equal? (pascal 3 2) 2)
+
+  (check-equal? (pascal 5 2) 4)
+  (check-equal? (pascal 5 3) 6)
+  (check-equal? (pascal 5 4) 4)
+  (check-equal? (pascal 5 5) 1))
